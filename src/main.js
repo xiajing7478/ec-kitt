4566b20cc2fa170c12b1ed1822f90afdb4a8cf61
xiajing7478 <260594736@qq.com>
2024-01-13 14:47:28 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { testApi } from '@/api/testApi'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+testApi().then((res) => {
+  console.log(res)
+})
 
 app.use(createPinia())
 app.use(router)
