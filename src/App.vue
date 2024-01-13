a3ff3541f3433391108a1187e0e3dcc1e96d02a8
xiajing7478 <260594736@qq.com>
2024-01-13 14:37:45 +0800

    主题色定制

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -16,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
+  <el-button type="primary">主要按钮</el-button>
   <RouterView />
 </template>
 
